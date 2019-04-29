27d0c9c20e19dcb1fde142654b18f4c6a950e51e
enjee <ngiarrusso@gmail.com>
2019-04-29 13:57:14 -0400

    Initial commit

---
+++ code.rb
@@ -0,0 +1,55 @@
+# Run the code. Change it so that it looks like the required output.
+
+require 'erb'
+require "pry"
+
+recipe = {
+  name: "Roasted Brussels Sprouts",
+  ingredients: [
+    "1 1/2 pounds Brussels sprouts",
+    "3 tablespoons good olive oil",
+    "3/4 teaspoon kosher salt",
+    "1/2 teaspoon freshly ground black pepper"
+  ],
+  directions: [
+    "Preheat oven to 400 degrees F.",
+    "Cut off the brown ends of the Brussels sprouts.",
+    "Pull off any yellow outer leaves.",
+    "Mix them in a bowl with the olive oil, salt and pepper.",
+    "Pour them on a sheet pan and roast for 35 to 40 minutes.",
+    "They should be until crisp on the outside and tender on the inside.",
+    "Shake the pan from time to time to brown the sprouts evenly.",
+    "Sprinkle with more kosher salt ( I like these salty like French fries).",
+    "Serve and enjoy!"
+  ]
+}
+
+recipe_title = "Recipe: #{recipe[:name]}"
+ingredients = ""
+
+
+
+recipe_template = <<-ERB
+
+#=<%= "=" * recipe_title.length %>=#
+# <%= recipe_title %> #
+#=<%= "=" * recipe_title.length %>=#
+
+Ingredients
+-----------
+
+<% recipe[:ingredients].each do |ingredient| %>
+<%= ingredient + '\n' %>
+<% end %>
+
+Directions
+-----------
+
+<% recipe[:directions].each_with_index do |step, index| %>
+<%= (index + 1).to_s + '. ' + step + '\n\n' %>
+<% end %>
+
+ERB
+
+erb = ERB.new(recipe_template, 0, '>')
+puts erb.result
